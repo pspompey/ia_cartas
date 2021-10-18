d38806924ab1f46a4fdde23947e768dd3119f890
pspompey <pompeypabl@frba.utn.edu.ar>
2021-10-18 14:38:06 -0300

    ruta

--- src/config.py
+++ src/config.py
@@ -8,5 +8,5 @@ config = {
     "alto_imagenes_a_procesar": 64,
     "epochs": 200,
     "tasa_aprendizaje": 0.001,
-    "archivo_weights": "C:\\Users\\pompe\\OneDrive\\Escritorio\\ia_blackjack\\src\\weights.h5"
+    "archivo_weights": "PATH\\ia_cartas\\src\\weights.h5"
 }
